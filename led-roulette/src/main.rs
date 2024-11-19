ec85157a60c596cc159e57680020b5a6a6a38a1a
Austin Vargason <vargasona@gmail.com>
2024-11-19 00:54:48 -0800

    working led roullete, modified for fun

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -2,13 +2,27 @@
 #![no_std]
 #![no_main]
 
-use aux5::entry;
+use volatile::Volatile;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    loop {}
+    let mut half_period = 50_u16;
+    let v_half_period = Volatile::new(&mut half_period);
+    const NUM_AHEAD: usize = 4;
+
+    loop {
+        for i in 0..8 {
+            for next_add in 1..(NUM_AHEAD+ 1) {
+               let next = (i + next_add) % 8;
+
+               leds[next].on().ok();
+            }
+            delay.delay_ms(v_half_period.read());
+            leds[i].off().ok();
+            delay.delay_ms(v_half_period.read());
+        }
+    }
 }
